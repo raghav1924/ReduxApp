2271f3d60eff35e7c39fad426d9ecf7e68386333
jatinsharma <raghavjatin245@gmail.com>
2023-07-24 15:16:04 +0530

    In between video 76

---
+++ src/state/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import amountManuplater from "./amountManuplater";
+
+export default reducers= combineReducers({
+    amount:amountManuplater
+})
---
+++ src/state/store.js
@@ -0,0 +1,5 @@
+import { createStoreHook } from "react-redux";
+import AmountManuplater from "./reducers/AmountManuplater";
+
+
+export const store =createStoreHook(reducers)
